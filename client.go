7af3e56a1661572db61acb71f75acf67e2f5b9b5
SeifAwadallah <58256655+SeifAwadallah@users.noreply.github.com>
2020-11-02 13:49:48 +0000

    Add files via upload

---
+++ client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+)
+
+func read(conn *net.Conn) {
+	//TODO In a continuous loop, read a message from the server and display it.
+	for {
+		reader := bufio.NewReader(*conn)
+		msg, _ := reader.ReadString('\n')
+		fmt.Println(msg)
+	}
+}
+
+func write(conn *net.Conn) {
+	//TODO Continually get input from the user and send messages to the server.
+	stdin := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("type message:")
+		text, _ := stdin.ReadString('\n')
+		fmt.Fprintln(*conn, text)
+	}
+}
+
+func main() {
+	// Get the server address and port from the commandline arguments.
+	addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
+	flag.Parse()
+	conn, _ := net.Dial("tcp", *addrPtr)
+	go read(&conn)
+	go write(&conn)
+	for {
+	}
+	//TODO Try to connect to the server
+	//TODO Start asynchronously reading and displaying messages
+	//TODO Start getting and sending user messages.
+}
